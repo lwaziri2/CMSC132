f5ae84173664ed21683de6cfd519604bd2497346
lwaziri2 <123435528+lwaziri2@users.noreply.github.com>
2023-05-03 00:45:28 -0400

    Add files via upload

---
+++ Thread Lab/CarQueue.java
@@ -0,0 +1,46 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+/**
+ * 
+ * @author Lima Waziri
+ *
+ */
+public class CarQueue {
+	private Queue<Integer> q;
+	private Random random = new Random();
+
+	public CarQueue() {
+		q = new LinkedList<>();
+		
+		q.add(random.nextInt(4));
+		q.add(random.nextInt(4));
+		q.add(random.nextInt(4));
+		q.add(random.nextInt(4));
+		q.add(random.nextInt(4));
+		q.add(random.nextInt(4));
+
+	}
+	
+	public void addToQueue() {
+		class AddRandom implements Runnable{
+
+			@Override
+			public void run() {
+				while (true) {
+                    q.add(random.nextInt(4));
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        new Thread(new AddRandom()).start();
+    }
+
+    public Integer deleteQueue() {
+        return q.remove();
+    }
+}
